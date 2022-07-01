1b0d99350ab1daf47d7f8f42d0fde329954e6062
Pier95F <forcinapierpaolo@gmail.com>
2022-07-01 09:46:31 +0200

    creata struttura classi

---
+++ src/jana60/eventi/Evento.java
@@ -0,0 +1,30 @@
+package jana60.eventi;
+
+public class Evento {
+
+/* La consegna è di creare una classe  Evento  che abbia  le seguenti proprietà: 
+ 	-   titolo 
+ 	-   data 
+ 	-   numero di posti totali (cioè la capienza della location) 
+ 	-   numero di posti prenotati 
+ 	
+   Quando si istanzia un nuovo evento questi attributi devono essere tutti valorizzati nel 
+   costruttore  , tranne posti prenotati   che va inizializzato a 0. 
+   
+   Inserire il controllo che la data non sia già passata e che il numero di posti totali sia un 
+   numero positivo. In caso contrario sollevare opportune eccezioni. 
+   
+   Aggiungere metodi  getter  e  setter  in modo che: 
+ 	-   titolo sia in lettura e in scrittura 
+ 	-   data sia in lettura e scrittura 
+ 	-   numero di posti totali sia solo in lettura 
+ 	-   numero di posti prenotati sia solo in lettura.
+ 	
+   Vanno inoltre implementati dei  metodi public  che svolgono  le seguenti funzioni: 
+ 	1.   prenota  : aggiunge uno ai posti prenotati. Se l’evento è già passato o non ha più 
+ 		 posti disponibili deve sollevare un’eccezione. 
+ 	2.   disdici  : riduce di uno i posti prenotati. Se l’evento  è già passato o non ci sono 
+ 		 prenotazioni deve sollevare un’eccezione. 
+ 	3.   l’override  del metodo  toString()  in modo che venga  restituita una stringa 
+ 		 contenente: data formattata - titolo.  */
+}
---
+++ src/jana60/eventi/Main.java
@@ -0,0 +1,20 @@
+package jana60.eventi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+/*  1.   Creare una classe  Main  di test, in cui si chiede all’utente  di inserire un nuovo evento 
+ 		 con tutti i parametri. 
+ 	2.   Dopo che l’evento è stato istanziato, chiedere all’utente se e quante prenotazioni 
+ 		 vuole fare e provare ad effettuarle, implementando opportuni controlli e gestendo 
+ 		 eventuali eccezioni. 
+ 	3.   Stampare a video il numero di posti prenotati e quelli disponibili 
+ 	4.   Chiedere all’utente se e quanti posti vuole disdire 
+ 	5.   Provare ad effettuare le disdette, implementando opportuni controlli e gestendo 
+ 		 eventuali eccezioni 
+ 	6.   Stampare a video il numero di posti prenotati e quelli disponibili. */
+
+	}
+
+}
