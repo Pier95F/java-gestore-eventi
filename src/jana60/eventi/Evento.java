4db6bf0735c7114e3c1b98cff5bd9863e93e6f97
Pier95F <forcinapierpaolo@gmail.com>
2022-07-01 10:34:35 +0200

    Milestone 1 completata

--- src/jana60/eventi/Evento.java
+++ src/jana60/eventi/Evento.java
@@ -1,5 +1,8 @@
 package jana60.eventi;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Evento {
 
 /* La consegna è di creare una classe  Evento  che abbia  le seguenti proprietà: 
@@ -27,4 +30,85 @@ public class Evento {
  		 prenotazioni deve sollevare un’eccezione. 
  	3.   l’override  del metodo  toString()  in modo che venga  restituita una stringa 
  		 contenente: data formattata - titolo.  */
+	
+	// Determino gli attributi della classe
+	private String titolo;
+	private LocalDate data;
+	private int nPostiTotali, nPostiPrenotati;
+	
+	// Costruttore con relative eccezioni
+	public Evento(String titolo, LocalDate data, int nPostiTotali, int nPostiPrenotati) throws Exception{
+		
+		// Controllo dei posti totali
+		if (nPostiTotali >0) {
+			this.nPostiTotali = nPostiTotali;
+		} else {
+			throw new Exception("Errore: il numero dei posti totali deve essere maggiore di 0.");
+		}
+		// Controllo della data
+		if (data.isAfter(LocalDate.now())) {
+			this.data = data;
+		} else {
+			throw new Exception("Errore: inserire una data valida.");
+		}
+		
+		this.titolo = titolo;
+		this.nPostiPrenotati = 0;
+	}
+
+	// Imposto i getter e i setter 
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public int getnPostiTotali() {
+		return nPostiTotali;
+	}
+
+	public int getnPostiPrenotati() {
+		return nPostiPrenotati;
+	}
+	
+	// Implementazione dei metodi
+	public void prenota() throws Exception {
+		if (data.isBefore(LocalDate.now())) {
+			throw new Exception("Errore: non è possibile prenotare eventi passati.");
+		} else if (nPostiTotali == nPostiPrenotati) {
+			throw new Exception("L'evento è già al completo, non ci sono posti disponibili.");
+		} else 
+			nPostiPrenotati ++;
+	}
+	
+	public void disdici() throws Exception {
+		if (data.isBefore(LocalDate.now())) {
+			throw new Exception("Errore: non è possibile disdire eventi passati.");
+		} else if (nPostiPrenotati <1) {
+			throw new Exception("Non ci sono prenotazioni effettuate.");
+		} else
+			nPostiPrenotati --;
+	}
+	
+	//Ovverride per la restituzione della stringa
+	@Override
+	public String toString() {
+		String dataFormattata;
+		DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		dataFormattata = this.data.format(df);
+		return this.titolo + " - " + dataFormattata;
+	}
+	
+	
 }
